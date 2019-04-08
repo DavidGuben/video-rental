2dd3d2d3adeae9fb8f8738f56ce0d5a3b0d76e00
David Guben <davidjg6@gmail.com>
2019-04-08 12:18:48 -0400

    moving to index

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
+
+// server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
