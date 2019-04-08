73165cc7c3ec7596fa860946d2530384712be67b
David Guben <davidjg6@gmail.com>
2019-04-08 12:30:52 -0400

    trying to add to server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const winston = require('winston');
 const express = require('express');
 const app = express();
+const path = require("path");
 
 require('./startup/logging')();
 require('./startup/routes')(app);
@@ -9,7 +10,11 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/production')(app);
 
+app.use(express.static(path.join(__dirname, "client", "build")));
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
--- startup/production.js
+++ startup/production.js
@@ -8,12 +8,5 @@ module.exports = function(app) {
     app.use(compression());
 
     // server static assets if in production
-  if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+  
 }
