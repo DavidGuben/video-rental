c5804ca64a77d282395e949f56b18df5b32244d0
David Guben <davidjg6@gmail.com>
2019-04-08 12:16:23 -0400

    cd ..

--- startup/routes.js
+++ startup/routes.js
@@ -22,5 +22,13 @@ module.exports = function(app) {
   app.use('/api/returns', returns);
   app.use(error);
 
-  
+  // server static assets if in production
+  if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('../client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 }
